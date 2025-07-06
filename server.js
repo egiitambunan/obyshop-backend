4b428b1b254c6f9d62c7e40453b8b71e88eb4c43
egiitambunan <144206075+egiitambunan@users.noreply.github.com>
2025-07-06 12:10:19 +0700

    server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ mongoose
   .then(() => {
     console.log("✅ MongoDB Connected");
 
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 8080;
     app.listen(PORT, () => {
       console.log(`🚀 Server running at PORT ${PORT}`);
     });
